390b0a6c9e878735d28e178388e3f7f05a05e0bf
alexei <oldlifeerror@gmail.com>
2024-05-06 01:31:28 +0300

    Update analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { ThemeProvider } from "styled-components";
 import App from "./App";
 import GlobalStyles from "./components/styles/Global";
 import theme from "./theme";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Analytics />
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <App />
